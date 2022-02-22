ab2e378ad7f36c4a46c26268734ec59ad5f1b791
mariahcboudreau <mariah.boudreau@uvm.edu>
2022-02-22 13:17:47 -0500

    adding winkler testing file

---
+++ winklerTesting.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 11:17:50 2022
+
+@author: mariahboudreau
+"""
+
+
+
+import numpy as np
+import pandas as pd
+import scipy
+import math
+import joypy
+import seaborn as sns
+from scipy import stats
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import networkx as nx
+from collections import Counter
+
+######
+#Checked on 1/19/22 - works
+######
+def derivative(p_kCoeff):
+    p_kCoeff = np.array(p_kCoeff)
+
+    
+    if p_kCoeff.size == 0:
+        return np.array([0])
+    elif p_kCoeff.size == 1:
+        return np.array([0])
+    else:
+        primeCoeff = np.empty((len(p_kCoeff)-1))
+        for p in range(len(p_kCoeff)-1):
+            primeCoeff[p] = p_kCoeff[p+1]*(p+1)
+        
+   
+    return primeCoeff 
+
+
+######
+# Checked on 1/20/22 - works
+######
+
+def ERCoeff(maxk, prob, n):
+    binomCoeff = []
+    
+    for k in range(maxk):
+        binomCoeff.append(scipy.special.binom(n-1, k)*prob**(k)*(1-prob)**(n-1-k))
+    
+    return binomCoeff
+
+
+#####
+# Checkin on 1/20/22 - works
+    
+# Used in the pgfOutbreak function
+#####
+
+def selfConsistant(g1):
+    #give dx
+    g1_prime = derivative(g1)
+    
+    # Make a copy of g1 in the right way
+    g1_copy = np.array([x for x in g1])
+    
+    if g1_copy.size > 1:
+        if g1_copy.size > 2: 
+            g1_copy[1] = g1_copy[1] - 1
+        elif g1_copy.size > 1:
+            g1_copy = np.append(g1_copy, -1)
+    else:
+        g1_copy = np.array([0,-1])
+    
+    
+    g1_copy = np.flip(g1_copy)
+
+    
+    if sum(g1_prime) > 1:
+        #print("There is a root below 1")
+        all_roots = np.roots(g1_copy)
+        
+        all_roots = all_roots[np.isreal(all_roots)]
+        all_roots = all_roots.real
+        all_roots = all_roots[all_roots >= 0]
+        all_roots = all_roots[all_roots < .99999999999]
+        if all_roots.size > 1:
+            all_roots = max(all_roots)
+        if all_roots.size == 0:
+            all_roots = 1
+        
+    else:
+        all_roots = 1
+    
+    return all_roots
+    
+    
+######
+# Checked on 1/20/22 - works
+######
+
+def pgfOutbreak(g1Outbreak):
+    
+    
+    root = selfConsistant(g1Outbreak)
+    
+    outbreak = 1-root
+    
+    return root, outbreak
+
+
+x = ERCoeff(10,0.5,10)
+
+r, o = pgfOutbreak(x)
+
+def winklerRevamp
