78309225daf95bc2a606350551082fb35e72edac
Mariah Boudreau <mcboudre@uvm.edu>
2024-05-23 15:57:48 -0400

    Commenting

--- scripts/test_laub_xia.py
+++ scripts/test_laub_xia.py
@@ -1,14 +1,14 @@
-from stochastic_pgfs.laub_xia_algo import kappa_SCE, is_real, in_bounds
+from stochastic_pgfs.laub_xia_algo import l_x_algo, is_real, in_bounds
 from stochastic_pgfs.pgfs import PGF, make_G_u_minus_u
 import numpy as np
 from scipy.stats import nbinom
 from scipy import special
 
 # Define the matrix size or retrieve it from existing data
-alpha_vals = np.linspace(0.1,1.0,10)  # Example values
+alpha_vals = np.linspace(0.1,0.2,10)  # Example values
 R0_vals = np.arange(1,5)  # Example values
 N_max = 10  # Maximum value for N in the distribution
-results = np.zeros((len(alpha_vals), len(R0_vals)))
+condition_nums = np.zeros((len(alpha_vals), len(R0_vals)))
 
 for i in range(len(alpha_vals)):
     for j in range(len(R0_vals)):
@@ -21,6 +21,8 @@ for i in range(len(alpha_vals)):
         # G = PGF(my_pdf)  # Generating function for the degree distribution
         # # Assuming giant_component_size and make_G_u_minus_u are properly implemented
         # G_u_minus_u = make_G_u_minus_u(G)
-        results[i, j] = kappa_SCE(my_pdf, K=100, conditions=[is_real, in_bounds], is_pgf = True)
+        condition_nums[i,j] = l_x_algo(my_pdf, K=10, conditions=[is_real, in_bounds], is_pgf = True)
 
-print(results)
+# print(true_roots)
+# print(perturbed_roots)
+print(condition_nums)
