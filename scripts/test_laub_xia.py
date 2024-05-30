66b523093437425a175fb18b3fd481e1cc9c1411
Mariah Boudreau <mcboudre@uvm.edu>
2024-05-30 14:58:38 -0400

    Simulation code done, contour plots done.

--- scripts/test_laub_xia.py
+++ scripts/test_laub_xia.py
@@ -2,11 +2,16 @@ from stochastic_pgfs.laub_xia_algo import l_x_algo, is_real, in_bounds
 from stochastic_pgfs.pgfs import PGF, make_G_u_minus_u
 import numpy as np
 from scipy.stats import nbinom
-from scipy import special
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime
+date = datetime.today().strftime('%m-%d-%Y')
+
 
 # Define the matrix size or retrieve it from existing data
-alpha_vals = np.linspace(0.1,0.2,10)  # Example values
-R0_vals = np.arange(1,5)  # Example values
+alpha_vals = np.arange(0.1,0.3,0.01)  # Example values
+R0_vals = np.arange(1,5, 0.1)  # Example values
 N_max = 10  # Maximum value for N in the distribution
 condition_nums = np.zeros((len(alpha_vals), len(R0_vals)))
 
@@ -21,8 +26,18 @@ for i in range(len(alpha_vals)):
         # G = PGF(my_pdf)  # Generating function for the degree distribution
         # # Assuming giant_component_size and make_G_u_minus_u are properly implemented
         # G_u_minus_u = make_G_u_minus_u(G)
-        condition_nums[i,j] = l_x_algo(my_pdf, K=10, conditions=[is_real, in_bounds], is_pgf = True)
+        condition_nums[i,j] = l_x_algo(my_pdf, K=1000, conditions=[is_real, in_bounds], is_pgf = True)
 
-# print(true_roots)
-# print(perturbed_roots)
-print(condition_nums)
+# df_variances = pd.DataFrame(condition_nums) 
+# sns.heatmap(df_variances, yticklabels= alpha_vals, xticklabels= R0_vals, cmap = "Blues")
+# plt.savefig("Users/mcboudre/Documents/LSD_Lab/stochastic_polys/stochastic_pgfs/plots/condition_number_heatmap"+ date +"_1000trials.pdf", format = "pdf")
+
+### Heat Map
+X, Y = np.meshgrid(R0_vals, alpha_vals)
+fig, ax = plt.subplots()
+levels = np.arange(0, 14000, 500)
+CS = ax.contourf(X, Y, condition_nums, levels = levels)
+ax.clabel(CS, inline=True, fontsize=10)
+ax.set_title('Condition numbers')
+plt.show()
+print("stop")
--- scripts/test_simulation.py
+++ scripts/test_simulation.py
@@ -1,28 +1,46 @@
-from stochastic_pgfs.laub_xia_algo import kappa_SCE, is_real, in_bounds
+from stochastic_pgfs.laub_xia_algo import l_x_algo, is_real, in_bounds
 from stochastic_pgfs.pgfs import PGF, make_G_u_minus_u
+import numpy as numpy
 from stochastic_pgfs.sim import *
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy.stats import nbinom
+from datetime import datetime
+date = datetime.today().strftime('%m-%d-%Y')
 
 
 # Define the matrix size or retrieve it from existing data
-alpha_vals = np.linspace(0.1,1.0,10)  # Example values
-R0_vals = np.arange(1,5)  # Example values
+alpha_vals = np.arange(0.1,0.3,0.01)  # Example values
+R0_vals = np.arange(1,5, 0.1)  # Example values
 N_max = 10  # Maximum value for N in the distribution
-results = np.zeros((len(alpha_vals), len(R0_vals)))
+variances = np.zeros((len(alpha_vals), len(R0_vals)))
+
+for i in range(len(alpha_vals)):
+    for j in range(len(R0_vals)):
+        # k = alpha_vals[i]
+        # R0 = R0_vals[j]
+        # coeffs = neg_binom(R0, k, N_max)
+        alpha = alpha_vals[i]
+        R0 = R0_vals[j]
+        r = 1 / alpha #dispersion parameter
+        p = alpha / (R0 + alpha) #Changed this parameter
+        dist = nbinom(n=r, p=p)  # Scipy parameterizes differently, check this
+        my_pdf = dist.pmf(range(N_max + 1))
+        variances[i,j] = variance_sim(my_pdf, trials = 1000, conditions=[is_real, in_bounds])
+
+### Heat Map
+X, Y = np.meshgrid(R0_vals, alpha_vals)
+fig, ax = plt.subplots()
+levels = np.arange(0, 0.4, 0.01)
+CS = ax.contourf(X, Y, variances, levels = levels)
+ax.clabel(CS, inline=True, fontsize=10)
+ax.set_title('Variances')
+plt.show()
+print("stop")
 
 
-# for i in range(len(alpha_vals)):
-#     for j in range(len(R0_vals)):
-#         alpha = alpha_vals[i]
-#         R0 = R0_vals[j]
-#         r = 1 / alpha #dispersion parameter
-#         p = alpha / (R0 + alpha) #Changed this parameter
-#         dist = nbinom(n=r, p=p)  # Scipy parameterizes differently, check this
-#         my_pdf = dist.pmf(range(N_max + 1))
-#         G = PGF(my_pdf)  # Generating function for the degree distribution
-#         # Assuming giant_component_size and make_G_u_minus_u are properly implemented
-#         G_u_minus_u = make_G_u_minus_u(G)
-#         results[i,j] = variance_polys(G_u_minus_u, K=10, conditions=[is_real, in_bounds], num_sims = 5)
 
-# print(results)
+df_variances = pd.DataFrame(variances) 
+sns.heatmap(df_variances, yticklabels= alpha_vals, xticklabels= R0_vals, cmap = "Greens")
+plt.savefig("Users/mcboudre/Documents/LSD_Lab/stochastic_polys/stochastic_pgfs/plots/variance_heatmap"+ date +"_1000trials.pdf", format = "pdf")
+print(variances)
