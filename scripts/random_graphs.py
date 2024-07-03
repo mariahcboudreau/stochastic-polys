e8a55af4b445407e99e158c8d9bdb73d6428a73f
WillHWThompson <will.w.thompson@gmail.com>
2024-07-03 10:54:26 -0400

    changes to rg script

--- scripts/random_graphs.py
+++ scripts/random_graphs.py
@@ -1,5 +1,6 @@
 from stochastic_pgfs.pgfs import PGF,make_G_u_minus_u,numerical_inversion,percolated_pgf
 from stochastic_pgfs.viz_utils import condition_number_heatmap
+from stochastic_pgfs.laub_xia_algo import l_x_algo, is_real, in_bounds
 import numpy as np
 from scipy.stats import nbinom
 import pandas as pd
@@ -23,20 +24,29 @@ def powerlaw_degree_sequence(alpha,N_max):
         degree_sequence[i] = i**(-alpha)
     return degree_sequence/sum(degree_sequence)
 
+def poisson_degree_sequence(lambd,n = 100):
+    #poisson distribution
+    degree_sequence = stats.poisson.pmf(np.arange(0,n),lambd)
+    return degree_sequence
+
 
 
 
 
 #generate ER graph
-n = 10#the size of the graph
-p = 0.214#density
-my_er_degree_sequence = er_degree_sequence(n = n, p = p)
+n = 100#the size of the graph
+lmbd = 2.#density
+alpha = 1.5
+my_er_degree_sequence = poisson_degree_sequence(lmbd)
+
+lx_addative = partial(l_x_algo, K=1000, conditions=[is_real, in_bounds],is_pgf=True,perturbation_type='additive')
+lx_addative(my_er_degree_sequence)
+
+
 er_pgf = PGF(my_er_degree_sequence)
 
-T =  1.0#infection rate
+T =  0.5#infection rate
 percolated_er_pgf =  partial(percolated_pgf,er_pgf,T = T)
-
-
 percolated_coef = numerical_inversion(percolated_er_pgf)
 
 lx_addative = partial(l_x_algo, K=1000, conditions=[is_real, in_bounds],is_pgf=True,perturbation_type='additive')
@@ -75,24 +85,25 @@ lx_multiplicative = partial(l_x_algo, K=1000, conditions=[is_real, in_bounds],is
 N,M = 5,5#resolution of the heatmap
 
 R0_vals = np.linspace(1,5,N)#
-p_vals = np.linspace(0.1,0.9,M)#density for the ER graph
+#p_vals = np.linspace(0,2,M)#density for the ER graph
+lmbd_vals = np.linspace(0,2,M)#density for the ER graph
 alpha_vals  = np.linspace(1,4,M)#power law exponent
 N_max = 100  # Maximum value for N in the distribution
 
 #create partial function for the condition number heatmap
 my_K = 1000
 
-er_degree_sequence_partial = partial(er_degree_sequence,n = N_max)
+poisson_degree_sequence_partial = partial(poisson_degree_sequence,n = N_max)
 powerlaw_degree_sequence_partial = partial(powerlaw_degree_sequence,N_max = N_max)
 
-er_condition_nums_addative= condition_num_heatmap(lx_addative,er_degree_sequence_partial,p_vals, R0_vals, N_max)
-powerlaw_condition_nums_addative= condition_num_heatmap(lx_addative,powerlaw_degree_sequence_partial,p_vals, R0_vals, N_max)
+er_condition_nums_addative= condition_num_heatmap(lx_addative,poisson_degree_sequence_partial,lmbd_vals, R0_vals, N_max)
+powerlaw_condition_nums_addative= condition_num_heatmap(lx_addative,powerlaw_degree_sequence_partial,alpha_vals, R0_vals, N_max)
 
 #### Heat Map
-X, Y = np.meshgrid(R0_vals, p_vals)
+X, Y = np.meshgrid(R0_vals, lmbd_vals)
 fig, ax = plt.subplots(ncols = 2,nrows =2,figsize = (10,5))
 ax[0,0].imshow(er_condition_nums_addative,
-            extent = (min(R0_vals),max(R0_vals),min(p_vals),max(p_vals)),
+            extent = (min(R0_vals),max(R0_vals),min(lmbd_vals),max(lmbd_vals)),
              aspect = 'auto'
             )
 ax[0,0].set(xlabel='R0 Values', ylabel='p Values', title='ER Additive Condition Numbers')
@@ -106,7 +117,7 @@ ax[1,0].set(xlabel='R0 Values', ylabel=r'$\alpha$ Values', title='Powerlaw Addit
 
 #
 #ax[1].imshow(condition_nums_multiplicative,
-#            extent = (min(R0_vals),max(R0_vals),min(p_vals),max(p_vals)),
+#            extent = (min(R0_vals),max(R0_vals),min(lmbd_vals),max(lmbd_vals)),
 #             aspect = 'auto'
 #            )
 #ax[1].set(xlabel='R0 Values', ylabel='Alpha Values', title='Multiplicative Condition Numbers')
