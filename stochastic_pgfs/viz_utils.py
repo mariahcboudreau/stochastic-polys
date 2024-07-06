b700f7e9ef8ec12bc7b9c7cfa0fe68ac3da83162
Mariah Boudreau <mcboudre@uvm.edu>
2024-07-06 11:15:38 -0400

    Mariah's updates for contour figures and NB condition numbers

--- stochastic_pgfs/viz_utils.py
+++ stochastic_pgfs/viz_utils.py
@@ -8,11 +8,27 @@ def condition_number_heatmap(lx_func, alpha_vals, R0_vals, N_max):
     for i in range(len(alpha_vals)):
         for j in range(len(R0_vals)):
             alpha = alpha_vals[i]
-            R0 = R0_vals[j]
-            r = 1 / alpha #dispersion parameter
+            n = alpha
+            R0 = R0_vals[j] # d is the dispersion parameter
             p = alpha / (R0 + alpha) #Changed this parameter
-            dist = nbinom(n=r, p=p)  # Scipy parameterizes differently, check this
+            dist = nbinom(n=n, p=p)  # Scipy parameterizes differently, check this
             my_pdf = dist.pmf(range(N_max + 1))
             my_pdf = my_pdf/np.sum(my_pdf)
             condition_nums[i,j] = lx_func(my_pdf)
     return condition_nums
+
+
+
+def outbreak_contours(solver, alpha_vals, R0_vals, N_max):
+    contours = np.zeros((len(alpha_vals), len(R0_vals)))
+    for i in range(len(alpha_vals)):
+        for j in range(len(R0_vals)):
+            alpha = alpha_vals[i]
+            n = alpha
+            R0 = R0_vals[j] # d is the dispersion parameter
+            p = alpha / (R0 + alpha) #Changed this parameter
+            dist = nbinom(n=n, p=p)  # Scipy parameterizes differently, check this
+            my_pdf = dist.pmf(range(N_max + 1))
+            my_pdf = my_pdf/np.sum(my_pdf)
+            contours[i,j] = solver(my_pdf)
+    return contours
