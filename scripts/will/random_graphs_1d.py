5afa7a82c48bf3eb24e37c8cc055fbe110b07a5e
WillHWThompson <will.w.thompson@gmail.com>
2024-11-21 15:20:46 -0500

    all code

--- scripts/will/random_graphs_1d.py
+++ scripts/will/random_graphs_1d.py
@@ -2,8 +2,8 @@ import numpy as np
 import timeit
 from stochastic_pgfs.pgfs import PGF,make_G_u_minus_u,numerical_inversion,percolated_pgf
 from stochastic_pgfs.viz_utils import condition_number_heatmap
-from stochastic_pgfs.laub_xia_algo import l_x_algo, is_real, in_bounds,_solve_self_consistent_equation
-from stochastic_pgfs.random_graphs import er_degree_sequence,powerlaw_degree_sequence,poisson_degree_sequence,make_config_model,power_law_variance,mean_power_law
+from stochastic_pgfs.laub_xia_algo import l_x_algo, is_real, in_bounds,_solve_self_consistent_equation,G1_prime,get_outbreak_size,iterate_until_convergence
+from stochastic_pgfs.random_graphs import powerlaw_degree_sequence,poisson_degree_sequence
 import numpy as np
 from scipy.stats import nbinom
 import pandas as pd
@@ -19,20 +19,10 @@ date = datetime.today().strftime('%m-%d-%Y')
 
 from multiprocessing import Pool
 
-
-def generate_T_values(control_param,dist_name,k_min = 1,k_max = 100):
-    if dist_name == 'poisson':
-        return 1/control_param
-    elif dist_name == 'powerlaw':
-        mean = mean_power_law(control_param,k_min, k_max)
-        variance = power_law_variance( control_param,k_min, k_max)
         
-        if np.isinf(variance):
-            # Handle infinite variance case
-            print("Variance is infinite for alpha <= 2. Returning critical point as zero.")
-            return 0.0  # Or handle as per your model requirements
-        else:
-            return mean / variance
+def calculate_critical_transition(my_degree_sequence):
+    pk = np.vstack((np.arange(0, my_degree_sequence.shape[0], 1), my_degree_sequence)).T
+    return 1/G1_prime(1, pk, 1)
         
         
 def process_data(lmbd, T,degree_sequence_func,lx_func):
@@ -41,34 +31,23 @@ def process_data(lmbd, T,degree_sequence_func,lx_func):
     sce_list = []
     outbreak_size_list = []
     my_degree_sequence = degree_sequence_func(lmbd)
-    #create the pgf for the degree sequence
-    my_pgf = PGF(my_degree_sequence)
-    #perform percolation on the degree sequece by composing the pgf with the (1-T)+Tx 
-    my_percolated_pgf = partial(percolated_pgf,my_pgf,T = T)
-    #invert the percolated pgf to get the coefficients
-    percolated_coef = numerical_inversion(my_percolated_pgf)
-    degree_sequence = percolated_coef
-    outbreak_size = 1-np.min(_solve_self_consistent_equation(degree_sequence))
-
-    return {'lmbd': lmbd, 'T': T, 'sce': lx_func(degree_sequence), 'outbreak_size': outbreak_size}
-
-
+    outbreak_size = get_outbreak_size(my_degree_sequence,T)
+    
+    return {'lmbd': lmbd, 'T': T, 'sce': lx_func(my_degree_sequence,T = T), 'outbreak_size': outbreak_size}
 
 
 N_max = 100  # Maximum value for N in the distribution
 my_K = int(1e5)#number of samples per SCE estimte
 
 #params to sweep over
-T_vals = np.linspace(0.001,1,60)
-alpha_vals = np.linspace(3.1,4,30)
-lmbd_vals = np.linspace(0.001,2,30)
+# T_vals = np.linspace(0.001,1,60)
+# alpha_vals = np.linspace(3.1,4,30)
+# lmbd_vals = np.linspace(0.001,2,30)
 
 
 T_vals = np.linspace(0.001,1,60)
 alpha_vals = np.array([3.1])
 lmbd_vals = np.array([1.2])
-
-
 
 
 #create partial function for the condition number heatmap for addative and multiplicative noise
@@ -97,12 +76,15 @@ if __name__ == '__main__':
                 results = []
                 for control_param in control_param_vals:
                     #add critical value to lise of T values
-                    critical_value = generate_T_values(control_param,dist_name,k_max=N_max)
+                    my_dist = dist_func(control_param)
+                    critical_value = calculate_critical_transition(my_dist)
+                    
                     T_vals_plus_crit = np.concatenate([T_vals,np.array([critical_value])])
                     T_vals_plus_crit = np.sort(T_vals_plus_crit)
                     T_vals_plus_crit = T_vals_plus_crit[(T_vals_plus_crit > 0) & (T_vals_plus_crit < 1)]#filter the critical occupation to ensure it is between 0 and 1
                     
                     for T in T_vals_plus_crit:
+                        
                         results.append(pool.apply_async(process_data, (control_param, T,dist_func,noise_func)) )
                         
                 data_dict_list = [result.get() for result in results]
